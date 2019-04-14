2c3add2ab23f535e0ae42d3ba249f4c5dbd8f475
kalasikk <kalasikk@gmail.com>
2019-04-14 11:50:32 +0300

    add 3 from 5 task of 03-date

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value)
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 
@@ -56,7 +56,8 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   var year=date.getFullYear();
+   return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
 
@@ -77,6 +78,14 @@ function isLeapYear(date) {
  */
 function timeSpanToString(startDate, endDate) {
    throw new Error('Not implemented');
+var hours = endDate.getHours()-startDate.getHours();
+
+var mins =  endDate.getMinutes()-startDate.getMinutes();
+
+
+var seconds = endDate.getSeconds()-startDate.getSeconds();
+var miliseconds=endDate.getMilliseconds()-startDate.getMilliseconds();
+return hours.toString().concat(':',mins).concat(':',seconds).concat(':',miliseconds);
 }
 
 
